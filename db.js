c1f9edb35bdb9e4aa64d8b67d700b85568ba2e3d
Ankit <ankitmaurya0225@gmail.com>
2021-07-18 13:02:09 +0530

    added Database

---
+++ db.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+
+const DataTypes = Sequelize.DataTypes
+
+const db = new Sequelize('shopping_cart_db', 'owner', 'shopping_covid',{
+    dialect: 'mysql',
+    host: 'localhost',
+    // How many Minimum Connections 
+    // So we will use Pool
+    pool: {
+        min: 0,
+        max: 5,
+    }
+})
+
+const User = db.define('owner' ,{
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+})
+
+const Products = db.define('products', {
+    
+})
