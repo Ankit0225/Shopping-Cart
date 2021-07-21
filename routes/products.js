994d7e059364c4f9d1d2a9789a887b72e573a533
Ankit <ankitmaurya0225@gmail.com>
2021-07-21 13:13:54 +0530

    issues

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,19 @@ const Product = require('../db').Product
 
 const route = require('express').Router();
 
+route.get('/', (req,res) => {
+    // Get your Products
 
+    Product.findAll()
+    .then((products) => {
+        res.status(200).send(products)
+    }).catch((err) => {
+        res.status(500).send({
+            error: "Unable to find products"
+        })
+    });
+})
+route.post('/', (req,res) => {
+//  Add A Product
+})
 exports = module.exports = route
